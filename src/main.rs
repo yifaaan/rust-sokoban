d6444ef609711152f1e58dc5ab686371e377cc52
Liuyifan <0219lyf@gmail.com>
2024-08-25 17:09:45 +0800

    define and register component

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use specs::{prelude::*, storage, Component};
+
+#[derive(Debug, Clone, Component)]
+#[storage(VecStorage)]
+pub struct Position {
+    x: u8,
+    y: u8,
+    z: u8,
+}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Renderable {
+    path: String,
+}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Wall {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Player {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Box {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct BoxSpot {}
+
 fn main() {
     println!("Hello, world!");
+}
+
+pub fn register_components(world: &mut World) {
+    // The `World` is our
+    // container for components
+    // and other resources.
+    world.register::<Position>();
+    world.register::<Renderable>();
+    world.register::<Wall>();
+    world.register::<Player>();
+    world.register::<Box>();
+    world.register::<BoxSpot>();
 }
