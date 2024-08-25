9a09ca7372675cc98f0eb9aa7f869028e331d936
Liuyifan <0219lyf@gmail.com>
2024-08-25 17:24:02 +0800

    create entity

--- src/main.rs
+++ src/main.rs
@@ -45,3 +45,60 @@ pub fn register_components(world: &mut World) {
     world.register::<Box>();
     world.register::<BoxSpot>();
 }
+
+/*
+* create entity
+*/
+pub fn create_wall(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "images/wall.png".to_string(),
+        })
+        .with(Wall {})
+        .build();
+}
+
+pub fn create_floor(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 5, ..position })
+        .with(Renderable {
+            path: "images/floor.png".to_string(),
+        })
+        .build();
+}
+
+pub fn create_box(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "images/box.png".to_string(),
+        })
+        .with(Box {})
+        .build();
+}
+
+pub fn create_box_spot(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 9, ..position })
+        .with(Renderable {
+            path: "images/box_spot.png".to_string(),
+        })
+        .with(BoxSpot {})
+        .build();
+}
+
+pub fn create_player(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "images/player.png".to_string(),
+        })
+        .with(Player {})
+        .build();
+}
