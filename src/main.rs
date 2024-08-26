9581adaef03318c11b39cde446c48a111ee930fc
Liuyifan <0219lyf@gmail.com>
2024-08-26 21:05:19 +0800

    test render entity

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use std::path;
 
 use ggez::{
     conf::{self, WindowMode},
-    event, Context, GameResult,
+    event,
+    glam::Vec2,
+    graphics::{self, Canvas, DrawParam, Drawable, Image},
+    Context, GameResult,
 };
-use specs::{prelude::*, storage, Component};
+use specs::{prelude::*, storage, world, Component};
 
+const TILE_WIDTH: f32 = 32.0;
 #[derive(Debug, Clone, Component)]
 #[storage(VecStorage)]
 pub struct Position {
@@ -56,7 +60,7 @@ pub fn create_wall(world: &mut World, position: Position) {
         .create_entity()
         .with(Position { z: 10, ..position })
         .with(Renderable {
-            path: "images/wall.png".to_string(),
+            path: "/images/wall.png".to_string(),
         })
         .with(Wall {})
         .build();
@@ -67,7 +71,7 @@ pub fn create_floor(world: &mut World, position: Position) {
         .create_entity()
         .with(Position { z: 5, ..position })
         .with(Renderable {
-            path: "images/floor.png".to_string(),
+            path: "/images/floor.png".to_string(),
         })
         .build();
 }
@@ -77,7 +81,7 @@ pub fn create_box(world: &mut World, position: Position) {
         .create_entity()
         .with(Position { z: 10, ..position })
         .with(Renderable {
-            path: "images/box.png".to_string(),
+            path: "/images/box.png".to_string(),
         })
         .with(Box {})
         .build();
@@ -88,7 +92,7 @@ pub fn create_box_spot(world: &mut World, position: Position) {
         .create_entity()
         .with(Position { z: 9, ..position })
         .with(Renderable {
-            path: "images/box_spot.png".to_string(),
+            path: "/images/box_spot.png".to_string(),
         })
         .with(BoxSpot {})
         .build();
@@ -99,7 +103,7 @@ pub fn create_player(world: &mut World, position: Position) {
         .create_entity()
         .with(Position { z: 10, ..position })
         .with(Renderable {
-            path: "images/player.png".to_string(),
+            path: "/images/player.png".to_string(),
         })
         .with(Player {})
         .build();
@@ -136,13 +140,41 @@ impl<'a> System<'a> for RenderingSystem<'a> {
 
     fn run(&mut self, data: Self::SystemData) {
         let (positions, renderables) = data;
-        unimplemented!();
+        // 清空背景
+        let mut canvas =
+            Canvas::from_frame(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
+
+        let mut rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
+        rendering_data.sort_by_key(|&k| k.0.z);
+
+        // Iterate through all pairs of positions & renderables, load the image
+        // and draw it at the specified position.
+        for (position, renderable) in rendering_data.iter() {
+            // load image
+            let image =
+                Image::from_path(self.context, renderable.path.clone()).expect("expect image");
+            let x = position.x as f32 * TILE_WIDTH;
+            let y = position.y as f32 * TILE_WIDTH;
+
+            // draw
+            let draw_params = DrawParam::new().dest(Vec2::new(x, y));
+            canvas.draw(&image, draw_params);
+        }
+        // present the context on the screen
+        canvas.finish(self.context).expect("expected to present");
     }
+}
+
+pub fn initialize_level(world: &mut World) {
+    create_player(world, Position { x: 0, y: 0, z: 0 });
+    create_wall(world, Position { x: 1, y: 0, z: 0 });
+    create_box(world, Position { x: 2, y: 0, z: 0 });
 }
 
 fn main() -> GameResult {
     let mut world = World::new();
     register_components(&mut world);
+    initialize_level(&mut world);
 
     let context_builder = ggez::ContextBuilder::new("rust_sokoban", "lyf")
         .window_setup(conf::WindowSetup::default().title("Rust Sokoban"))
