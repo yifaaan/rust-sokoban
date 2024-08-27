6c35ef74537633944b6407beb60a54cee606715e
Liuyifan <0219lyf@gmail.com>
2024-08-27 09:11:09 +0800

    move box and player

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path;
+use std::{collections::HashMap, path};
 
 use ggez::{
     conf::{self, WindowMode},
@@ -11,7 +11,11 @@ use ggez::{
 use specs::{prelude::*, storage, world, Component};
 
 /// item 大小系数
-const TILE_WIDTH: f32 = 30.0;
+const TILE_WIDTH: f32 = 32.0;
+
+const MAP_HEIGHT: u8 = 9;
+const MAP_WIDTH: u8 = 8;
+
 #[derive(Debug, Clone, Component)]
 #[storage(VecStorage)]
 pub struct Position {
@@ -42,6 +46,14 @@ pub struct Box {}
 #[storage(VecStorage)]
 pub struct BoxSpot {}
 
+#[derive(Component, Default)]
+#[storage(NullStorage)]
+pub struct Movable;
+
+#[derive(Component, Default)]
+#[storage(NullStorage)]
+pub struct Immovable;
+
 pub fn register_components(world: &mut World) {
     // The `World` is our
     // container for components
@@ -52,6 +64,8 @@ pub fn register_components(world: &mut World) {
     world.register::<Player>();
     world.register::<Box>();
     world.register::<BoxSpot>();
+    world.register::<Movable>();
+    world.register::<Immovable>();
 }
 
 pub fn register_resources(world: &mut World) {
@@ -67,6 +81,7 @@ pub fn create_wall(world: &mut World, position: &Position) {
         .with(Renderable {
             path: "/images/wall.png".to_string(),
         })
+        .with(Immovable {})
         .with(Wall {})
         .build();
 }
@@ -88,6 +103,7 @@ pub fn create_box(world: &mut World, position: &Position) {
         .with(Renderable {
             path: "/images/box.png".to_string(),
         })
+        .with(Movable {})
         .with(Box {})
         .build();
 }
@@ -110,6 +126,7 @@ pub fn create_player(world: &mut World, position: &Position) {
         .with(Renderable {
             path: "/images/player.png".to_string(),
         })
+        .with(Movable {})
         .with(Player {})
         .build();
 }
@@ -261,19 +278,77 @@ pub struct InputSystem {}
 impl<'a> System<'a> for InputSystem {
     type SystemData = (
         Write<'a, InputQueue>,
+        Entities<'a>,
         WriteStorage<'a, Position>,
         ReadStorage<'a, Player>,
+        ReadStorage<'a, Movable>,
+        ReadStorage<'a, Immovable>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut input_queue, mut positions, players) = data;
-        for (position, _player) in (&mut positions, &players).join() {
+        let (mut input_queue, entities, mut positions, players, movables, immovables) = data;
+
+        let mut to_move = Vec::new();
+        for (position, _player) in (&positions, &players).join() {
             if let Some(key) = input_queue.keys_pressed.pop() {
+                // (x, y) --> id
+                let mov = (&entities, &movables, &positions)
+                    .join()
+                    .map(|t| ((t.2.x, t.2.y), t.0.id()))
+                    .collect::<HashMap<_, _>>();
+                let immov = (&entities, &immovables, &positions)
+                    .join()
+                    .map(|t| ((t.2.x, t.2.y), t.0.id()))
+                    .collect::<HashMap<_, _>>();
+
+                // 在该方向从起点迭代到地图的边界
+                let (start, end, is_x) = match key {
+                    KeyCode::Up => (position.y, 0, false),
+                    KeyCode::Down => (position.y, MAP_HEIGHT, false),
+                    KeyCode::Left => (position.x, 0, true),
+                    KeyCode::Right => (position.x, MAP_WIDTH, true),
+                    _ => continue,
+                };
+
+                let range = if start < end {
+                    (start..=end).collect::<Vec<_>>()
+                } else {
+                    (end..=start).rev().collect::<Vec<_>>()
+                };
+
+                // 遍历路径上的每一个点
+                for x_or_y in range {
+                    let pos = if is_x {
+                        (x_or_y, position.y)
+                    } else {
+                        (position.x, x_or_y)
+                    };
+
+                    match mov.get(&pos) {
+                        // entity
+                        Some(id) => to_move.push((key, id.clone())),
+                        None => {
+                            // immovable
+                            match immov.get(&pos) {
+                                // 如果路径上有一个Immovable，则停止移动所有
+                                Some(_id) => to_move.clear(),
+                                // floor
+                                None => break,
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // 移动所有可移动物
+        for (key, id) in to_move {
+            let position = positions.get_mut(entities.entity(id));
+            if let Some(p) = position {
                 match key {
-                    KeyCode::Up => position.y -= 1,
-                    KeyCode::Down => position.y += 1,
-                    KeyCode::Left => position.x -= 1,
-                    KeyCode::Right => position.x += 1,
+                    KeyCode::Up => p.y -= 1,
+                    KeyCode::Down => p.y += 1,
+                    KeyCode::Left => p.x -= 1,
+                    KeyCode::Right => p.x += 1,
                     _ => (),
                 }
             }
