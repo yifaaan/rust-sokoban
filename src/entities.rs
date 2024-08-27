670b920ef6a6cf7914e931fd17e24f8a4e7f2d0f
lyfQAQ <0219lyf@gmail.com>
2024-08-27 14:44:53 +0800

    entity creation

--- src/entities.rs
+++ src/entities.rs
@@ -6,9 +6,7 @@ pub fn create_wall(world: &mut World, position: &Position) {
     world
         .create_entity()
         .with(Position { z: 10, ..*position })
-        .with(Renderable {
-            path: "/images/wall.png".to_string(),
-        })
+        .with(Renderable::new_static("/images/wall.png".to_string()))
         .with(Immovable {})
         .with(Wall {})
         .build();
@@ -18,9 +16,7 @@ pub fn create_floor(world: &mut World, position: &Position) {
     world
         .create_entity()
         .with(Position { z: 5, ..*position })
-        .with(Renderable {
-            path: "/images/floor.png".to_string(),
-        })
+        .with(Renderable::new_static("/images/floor.png".to_string()))
         .build();
 }
 
@@ -28,9 +24,11 @@ pub fn create_box(world: &mut World, position: &Position, color: BoxColor) {
     world
         .create_entity()
         .with(Position { z: 10, ..*position })
-        .with(Renderable {
-            path: format!("/images/box_{}.png", color),
-        })
+        .with(Renderable::new_animated(vec![
+            format!("/images/box_{}_1", color),
+            format!("/images/box_{}_2", color),
+            format!("/images/box_{}_3", color),
+        ]))
         .with(Movable {})
         .with(Box { color })
         .build();
@@ -40,9 +38,10 @@ pub fn create_box_spot(world: &mut World, position: &Position, color: BoxColor) 
     world
         .create_entity()
         .with(Position { z: 9, ..*position })
-        .with(Renderable {
-            path: format!("/images/box_spot_{}.png", color),
-        })
+        .with(Renderable::new_static(format!(
+            "/images/box_spot_{}.png",
+            color
+        )))
         .with(BoxSpot { color })
         .build();
 }
@@ -51,9 +50,11 @@ pub fn create_player(world: &mut World, position: &Position) {
     world
         .create_entity()
         .with(Position { z: 10, ..*position })
-        .with(Renderable {
-            path: "/images/player.png".to_string(),
-        })
+        .with(Renderable::new_animated(vec![
+            "/images/player_1.png".to_string(),
+            "/images/player_2.png".to_string(),
+            "/images/player_3.png".to_string(),
+        ]))
         .with(Movable {})
         .with(Player {})
         .build();
